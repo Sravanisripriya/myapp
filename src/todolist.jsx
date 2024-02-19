45a0c1debf6858b25dec6ee05f970a359c0f46e7
sravanisripriya <sravanisripriya93@gmail.com>
2024-02-19 16:30:22 +0530

    basic todo

---
+++ src/todolist.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Todolist(){
+
+    return(
+        <div>
+            <input type="text" id="task"/>
+            <button >Add</button>
+        </div>
+    )}
+
+    export default Todolist
+    
