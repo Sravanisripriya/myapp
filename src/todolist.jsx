4d7a4843bfc602b043716dc49bfcdce5a0e256f4
sravanisripriya <sravanisripriya93@gmail.com>
2024-02-20 19:25:54 +0530

    del done

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useRef } from "react";
 
 function Todolist(){
+    let empty=useRef()
+    let [strike,setStrike]=React.useState(false)
     var [task,setTask]=React.useState([])
     var[newtask,setNewtask]=React.useState("")
       function add(){
          setTask([...task,newtask])
+         empty.current.value=""
       }
       function del(index){
           var temp=[...task]
         temp.splice(index,1)
         setTask([...temp])
       }
+      const done=(index)=>{
+       
+      }
     return(
         <div>
             <h1>TODOLIST</h1>
-            <input type="text" onKeyUp={(e)=>{setNewtask(e.target.value)}}/>
+            <input type="text"  ref={empty} onKeyUp={(e)=>{setNewtask(e.target.value)}}/>
             <button onClick={()=>{add()}} >Add</button>
             <ul>
             {task.map((todos,i)=>{
                 return(
-                    <li>{todos}
+                    <li style={strike?{textDecoration:"line-through"}:{}}>{todos}
                     <button onClick={()=>{del(i)}}>Delete</button>
+                    <button onClick={()=>{done(i)}}>Done</button>
                     </li>
                 )
             })}
@@ -30,4 +37,7 @@ function Todolist(){
     )}
 
     export default Todolist
+    
+
+
     
