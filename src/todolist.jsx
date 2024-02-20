e35758cf4a08cb44f2a1a060e46fc790b9ad3c3f
sravanisripriya <sravanisripriya93@gmail.com>
2024-02-20 18:13:16 +0530

    todo

--- src/todolist.jsx
+++ src/todolist.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 function Todolist(){
-
+    var [task,setTask]=React.useState([])
+    var[newtask,setNewtask]=React.useState("")
+      function add(){
+         setTask([...task,newtask])
+      }
+      function del(index){
+          var temp=[...task]
+        temp.splice(index,1)
+        setTask([...temp])
+      }
     return(
         <div>
-            <input type="text" id="task"/>
-            <button >Add</button>
+            <h1>TODOLIST</h1>
+            <input type="text" onKeyUp={(e)=>{setNewtask(e.target.value)}}/>
+            <button onClick={()=>{add()}} >Add</button>
+            <ul>
+            {task.map((todos,i)=>{
+                return(
+                    <li>{todos}
+                    <button onClick={()=>{del(i)}}>Delete</button>
+                    </li>
+                )
+            })}
+            </ul>
+
         </div>
     )}
 
