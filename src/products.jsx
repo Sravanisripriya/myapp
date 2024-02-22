c2dc80800480255f4aa1a4097bb8e1ae8a7a1b0d
sravanisripriya <sravanisripriya93@gmail.com>
2024-02-22 23:12:08 +0530

    cart

--- src/products.jsx
+++ src/products.jsx
@@ -26,27 +26,26 @@ function Products(){
         
 
     return(
-        <div className="d-flex">
-         <div className="d-flex flex-wrap w-75">
+        <div className="d-flex" >
+         <div className="d-flex flex-wrap w-75" >
             {
                 products?.map((product)=>{
                   return(
                    
-                        <div className="card  m-3 " style={{width:"250px"}}>
-                        <img src={product.image} class="card-img-top" alt="..."/>
-                        <div class="card-body d-flex flex-column justify-content-between">
-                            <h5 class="card-title">{product.title}</h5>
+                        <div className="card  m-4 " style={{width:"200px"}}  >
+                        <img src={product.image} class="card-img-top" id="img" />
+                        <div class="card-body d-flex flex-column justify-content-between" >
+                            <h4 class="card-title">{product.title}</h4>
                             <p class="card-text"><small class="text-body-secondary">{product.category}</small></p>
-                            <button className=" btn btn-success" onClick={()=>{addToCart(product)}}>Add to cart</button>
+                            <button className="btn btn-primary" onClick={()=>{addToCart(product)}}>Add to cart</button>
                         </div>
-                   
                     </div>
 
                   )
                 })
             }
      </div>
-     <div className="border border-dark w-25">
+     <div className="border border-dark w-25" style={{backgroundColor:"pink"}}>
         <Cart cartItems={cartItems}></Cart>
         </div>
      </div>
