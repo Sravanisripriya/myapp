9fcb56e94efd047bfebe40b3d94f0ebb77223564
sravanisripriya <sravanisripriya93@gmail.com>
2024-02-26 16:52:44 +0530

    routing

---
+++ src/counter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Counter() {
+  return (
+    <div className="App">
+    <h1> this is a Counter</h1>
+    </div>
+  );
+}
+
+export default Counter;
---
+++ src/todos.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Todo() {
+  return (
+    <div className="App">
+    <h1> this is a Todo</h1>
+    </div>
+  );
+}
+
+export default Todo;
