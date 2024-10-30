e303b78274cbeaeb299aa8300b74e595ef6fb244
sravanisripriya <sravanisripriya93@gmail.com>
2024-10-30 12:11:59 +0530

    cmplt

--- src/formik.jsx
+++ src/formik.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
-import * as  Yup from 'yup'
+
 
 function checkForm(values){                  
 // console.log("checkForm::",values)         
